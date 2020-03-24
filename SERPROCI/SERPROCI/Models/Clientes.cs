7e13d4823237a2c7161e37e24f42fc0354229be9
yerson lugo <yerson.lugo10@gmail.com>
2020-03-24 14:57:19 -0600

    crear tabla cliente

---
+++ SERPROCI/SERPROCI/Models/Clientes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace SERPROCI.Models
+{
+
+    public class Clientes
+    {
+    }
+}
