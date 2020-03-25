e57e794bb2db4769f1efa4b375fa39f2d2156904
yerson lugo <yerson.lugo10@gmail.com>
2020-03-25 07:50:30 -0600

    Creando MasterPedido

---
+++ SERPROCI/SERPROCI/Models/MasterPedido.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace SERPROCI.Models
+{
+    [Table("MasterPedido")]
+    public class MasterPedido
+    {
+        [Key]
+        [Display(Name = "Id Pedido")]
+        public int IdMasterPedido { get; set; }
+
+        [Display(Name = "Id Cliente")]
+        public int IdCliente { get; set; }
+
+        [Display(Name = "Id TipoPedido")]
+        public int IdTipoPedido { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha Pedido")]
+        public DateTime FechaPedido { get; set; }
+
+        [Display(Name = "Concepto")]
+        [StringLength(300)]
+        [Required(ErrorMessage = "El Campo {0} Es Requqerido")]
+        public string Concepto { get; set; }
+
+        [Display(Name = "Estado")]
+        [StringLength(20)]
+        [Required(ErrorMessage = "El Campo {0} Es Requqerido")]
+        public string Estado { get; set; }
+
+        [Display(Name = "Anulado")]
+        public bool? Anulado { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha Anulado")]
+        public DateTime FechaAnulado { get; set; }
+
+
+        [ForeignKey("IdTipoPedido")]
+        public virtual TipoPedido TipoPedido { get; set; }
+    }
+}
