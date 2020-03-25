f4787f179275259012f07e164c20343f7060dc63
yerson lugo <yerson.lugo10@gmail.com>
2020-03-25 08:57:14 -0600

    DetallePedido

---
+++ SERPROCI/SERPROCI/Models/DetallePedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace SERPROCI.Models
+{
+    [Table("DetallePedido")]
+    public class DetallePedido
+    {
+        [Key]
+        [Display(Name = "Id Detalle Pedido")]
+        public int IdDetallePedido { get; set; }
+
+        [Display(Name = "Id ProSer")]
+        public int IdProser { get; set; }
+
+    }
+}
