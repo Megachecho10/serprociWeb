a51ae96e511d84742b02064e0237c3440a8e5324
yerson lugo <yerson.lugo10@gmail.com>
2020-03-25 17:15:27 -0600

    Master Factura

---
+++ SERPROCI/SERPROCI/Models/MasterFactura.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace SERPROCI.Models
+{
+    [Table("MasterFactura")]
+    public class MasterFactura
+    {
+
+    }
+}
